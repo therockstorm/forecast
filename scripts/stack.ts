0133a684a585f428293c69f997626aae0baba208
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2020-04-20 10:29:13 -0600

    Update deps, fix env vars

--- scripts/stack.ts
+++ scripts/stack.ts
@@ -25,10 +25,10 @@ class MyStack extends Stack {
           },
           FORECAST_EMAIL: { value: envVar("FORECAST_EMAIL") },
           FORECAST_PHONE_NUMBER_FROM: {
-            value: envVar("FORECAST_PHONE_NUMBER_FROM"),
+            value: process.env.FORECAST_PHONE_NUMBER_FROM || "",
           },
           FORECAST_PHONE_NUMBER_TO: {
-            value: envVar("FORECAST_PHONE_NUMBER_TO"),
+            value: process.env.FORECAST_PHONE_NUMBER_TO || "",
           },
           FORECAST_LAT: { value: envVar("FORECAST_LAT") },
           FORECAST_LON: { value: envVar("FORECAST_LON") },
@@ -38,14 +38,15 @@ class MyStack extends Stack {
           },
           SERVERLESS_ACCESS_KEY: { value: envVar("SERVERLESS_ACCESS_KEY") },
           STAGE: { value: STAGE },
-          TWILIO_ACCOUNT_SID: { value: envVar("TWILIO_ACCOUNT_SID") },
-          TWILIO_AUTH_TOKEN: { value: envVar("TWILIO_AUTH_TOKEN") },
+          TWILIO_ACCOUNT_SID: { value: process.env.TWILIO_ACCOUNT_SID || "" },
+          TWILIO_AUTH_TOKEN: { value: process.env.TWILIO_AUTH_TOKEN || "" },
         },
       },
       source: Source.gitHub({ owner: "therockstorm", repo: name }),
       timeout: Duration.minutes(5),
     })
   }
+  j
 }
 
 const app = new App()
